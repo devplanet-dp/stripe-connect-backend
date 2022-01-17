58751018e4a9159d946c46c6b9aad84475f038c2
DEV PLANET <59248870+devplanet-dp@users.noreply.github.com>
2022-01-17 11:23:47 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Panda Gums</title>
+        <title>Ridershare</title>
         <link rel="icon" href="/favicon.ico" />
         <script src="https://js.stripe.com/v3/"></script>
       </Head>
@@ -18,28 +18,6 @@ function MyApp({ Component, pageProps }) {
       <div className="app">
         <Header></Header>
         <Component {...pageProps} />
-        <footer className="footer">
-          <a
-            href="https://twitter.com/muhd_ahsanayaz"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Built by
-            <img
-              src="/profile-image.jpeg"
-              alt="Profile Image"
-              className="logo"
-            />{" "}
-            Muhammad Ahsan Ayaz with ❤️
-          </a>
-          <a
-            title="Like Ahsan's work? Buy him a coffee"
-            className="bmac"
-            href="https://www.buymeacoffee.com/muhd.ahsanayaz"
-          >
-            <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=muhd.ahsanayaz&button_colour=BD5FFF&font_colour=ffffff&font_family=Comic&outline_colour=000000&coffee_colour=FFDD00" />
-          </a>
-        </footer>
       </div>
     </>
   )
