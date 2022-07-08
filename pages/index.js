15f5a44d69cc229296ed88a37ee653e01d0df986
Kasun Niroshana <zapniroshana420@gmail.com>
2022-07-08 19:34:06 +0530

    -update-home

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,21 @@ export default function Home() {
   return (
     <div className="container">
       <main className="main">
-      <Link href="/register">
+        <div className={styles.grid}>
+          <Link href="/register">
             <div className={styles.card}>
               <h3>Register to receive payments &rarr;</h3>
               <p>Register yourself as a service provider</p>
             </div>
           </Link>
+
+          {/* <Link href="/pay-out">
+            <div className={styles.card}>
+              <h3>Pay as a Customer &rarr;</h3>
+              <p>Pay as a customer to the service provider</p>
+            </div>
+          </Link> */}
+        </div>
       </main>
     </div>
   )
